0bfde97a62126629e3bb6afe8d9029d84599f875
Bishop Bader <bflatbader@users.noreply.github.com>
2019-09-10 23:17:47 -0400

    added comments

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// FUNCTIONS
-
 // VARIABLES
 var apiKey = "Wmr6S0oVwYJDKCeFjAyhWiu9hEkgb9U4";
 var queryURL = "";
@@ -7,6 +5,7 @@ var numRecords = "";
 var startYear = "";
 var endYear = "";
 
+// Clear all fields on Clear click
 $('#clearBtn').on("click", function(event) {
     search = $('#searchTerm').val("");
     numRecords = $('#records').val("");
@@ -15,20 +14,23 @@ $('#clearBtn').on("click", function(event) {
     $('li').remove();
 });
 
+// When the Search button is clicked
 $('#searchBtn').on("click", function(event) {
+    // Get values from the form fields
     search = $('#searchTerm').val();
     numRecords = $('#records').val();
     startYear = $('#yearOne').val();
     endYear = $('#yearTwo').val();
 
+    // Set query URL
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
         search + "&api-key=" + apiKey;
-
+    // Add to query URL if a Start Year is entered
     if (startYear) { 
         beginDate = startYear + "0101";
         queryURL = queryURL + "&facet_fields=source&facet=true&begin_date=" + beginDate;
     } 
-    
+    // Add to query URL if an End Year is entered
     if (endYear) {
         endDate = endYear + "0101";
         queryURL = queryURL + "&end_date=" + endDate;
@@ -43,9 +45,10 @@ $('#searchBtn').on("click", function(event) {
         })
         // After data comes back from the request
         .then(function(results) {
-            // console.log(results.response.docs[0].abstract);
+            // If the user entered a Number of Records to Retrieve
             if(numRecords) {
                 for (i = 0; i < numRecords; i++) {
+                    // Get the article information
                     article = results.response.docs[i];
                     console.log(article);
                     title = article.headline.main;
@@ -54,6 +57,7 @@ $('#searchBtn').on("click", function(event) {
                     url = article.web_url;
                     pubDate = article.pub_date;
 
+                    // Setup new li element and append to the #results div
                     var listItem = $("<li><h4>" + title + "</h4><br>"
                     + "<p>" + byline + "</p>"
                     + "<p>" + pubDate + "</p>"
@@ -64,7 +68,9 @@ $('#searchBtn').on("click", function(event) {
                     $('#results').append(listItem);
                 } 
             } else {
+                // No Number of Records entered, retrieve 10 since that is the max per API
                 for (i = 0; i < 10; i++) {
+                    // Get the article information
                     article = results.response.docs[i];
                     console.log(article);
                     title = article.headline.main;
@@ -73,6 +79,7 @@ $('#searchBtn').on("click", function(event) {
                     url = article.web_url;
                     pubDate = article.pub_date;
 
+                    // Setup new li element and append to the #results div
                     var listItem = $("<li><h4>" + title + "</h4><br>"
                     + "<p>" + byline + "</p>"
                     + "<p>" + pubDate + "</p>"
