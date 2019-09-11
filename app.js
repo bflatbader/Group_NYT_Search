026723db3639e2627b5227bbed32544ecdf7ce71
Bishop Bader <bflatbader@gmail.com>
2019-09-10 21:39:48 -0400

    initial upload

---
+++ app.js
@@ -0,0 +1,61 @@
+// FUNCTIONS
+
+// VARIABLES
+var apiKey = "Wmr6S0oVwYJDKCeFjAyhWiu9hEkgb9U4";
+var queryURL = "";
+var numRecords = "";
+var startYear = "";
+var endYear = "";
+
+$('#searchBtn').on("click", function(event) {
+    search = $('#searchTerm').val();
+    numRecords = $('#records').val();
+    startYear = $('#yearOne').val();
+    endYear = $('#yearTwo').val();
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+        search + "&api-key=" + apiKey;
+
+    if (startYear) { 
+        beginDate = startYear + "0101";
+        queryURL = queryURL + "&facet_fields=source&facet=true&begin_date=" + beginDate;
+    } 
+    
+    if (endYear) {
+        endDate = endYear + "0101";
+        queryURL = queryURL + "&end_date=" + endDate;
+    }
+
+    console.log(queryURL);
+
+    // AJAX Request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        // After data comes back from the request
+        .then(function(results) {
+            // console.log(results.response.docs[0].abstract);
+            if(numRecords) {
+                for (i = 0; i < numRecords; i++) {
+                    article = results.response.docs[i];
+                    title = article.headline.main;
+                    byline = article.byline.original;
+                    section = article.section_name;
+                    url = article.web_url;
+                    pubDate = article.pub_date;
+                } 
+            } else {
+                for (i = 0; i < 10; i++) {
+                    article = results.response.docs[i];
+                    title = article.headline.main;
+                    byline = article.byline.original;
+                    section = article.section_name;
+                    url = article.web_url;
+                    pubDate = article.pub_date;
+                }
+            }
+
+        });
+});
+
